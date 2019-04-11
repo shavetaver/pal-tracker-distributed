67ebd31be568e202edc0d5a73aaebf55656c8e05
shavetaverma112@gmail.com <shavetaverma112@gmail.com>
2019-04-11 13:20:53 -0700

    spring cloud | Lab 2

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/SpringDefaultController.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/SpringDefaultController.java
@@ -3,6 +3,7 @@ package io.pivotal.pal.tracker.restsupport;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class SpringDefaultController {
 
